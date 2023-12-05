57eae5bee3f8f60142b39e3225b5a415e7123b6f
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-12-05 22:16:42 +0100

    refactor(mongodb): Refactor MongoDB connection and database initialization
    The changes in this commit refactor the code related to MongoDB connection and database initialization in the `mongodb.py` file. The code has been organized into a separate `Database` class and the initialization process has been improved to handle multiple URIs. Additionally, the `utils/connection.py` file has been modified to remove the `Database` class, as it has been moved to the `mongodb.py` file.

--- mongodb.py
+++ mongodb.py
@@ -1,44 +1,128 @@
-from flask import Flask, request, render_template, redirect, url_for
+import atexit
+import logging
+import os
 
-from utils.connection import Database
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure, PyMongoError
 
-app = Flask(__name__)
-db = Database()  # Instantiate the Database class
+from utils.connection import database_connection
 
-
-@app.route('/<string:collection>/<string:document>/', methods=['GET'])
-@app.route('/<string:collection>/<string:document>/<string:action>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def manage_document(collection, document, action='READ'):
-    context = {
-        'collection': collection,
-        'document': document,
-        'action': action,
-        'fields': request.args,
-        'document_data': None
-    }
-
-    if action == 'CREATE' and request.method == 'POST':
-        # Logic to handle document creation
-        db.create_document(collection, request.form.to_dict())
-        return redirect(url_for('manage_document', collection=collection, document='new', action='READ'))
-
-    elif action == 'READ':
-        # Logic to read and display a document
-        raise NotImplementedError
-        # context['document_data'] = db.read_document(document, collection)
-
-    elif action == 'UPDATE' and request.method == 'POST':
-        # Logic to update a document
-        db.update_document(collection, document, request.form.to_dict())
-        return redirect(url_for('manage_document', collection=collection, document=document, action='READ'))
-
-    elif action == 'DELETE' and request.method == 'POST':
-        # Logic to delete a document
-        db.delete_document(collection, document)
-        return redirect(url_for('manage_document', collection=collection, document='new', action='READ'))
-
-    return render_template('documents/document_manager.html', **context)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+class Database:
+    def __init__(self, database_name="tasktracker"):
+        self.db_name = database_name
+        self.client = None
+        self.collections = []
+        self.initialize_db()
+
+    def initialize_db(self, uris=None):
+        if self.client:
+            return self.client
+
+        if uris is None:
+            uris = [
+                os.getenv('MONGO_URI'),
+                f"mongodb://localhost:27017/{self.db_name}",
+            ]
+
+        uri = uris[0]
+        if not uri:
+            logger.warning("No valid URI found in the current attempt, trying the next option...")
+            self.initialize_db(uris[1:])
+            return
+
+        try:
+            self.client = MongoClient(uri)
+            self.client.admin.command('ping')
+            self.collections = self.client[self.db_name].list_collection_names()
+            atexit.register(self.close_connection)
+            logger.info(f"Connected to MongoDB using URI: {uri}")
+        except ConnectionFailure:
+            logger.error(f"Failed to connect to MongoDB using URI: {uri}, trying the next option...")
+            self.initialize_db(uris[1:])
+        except IndexError:
+            logger.warning("No valid URI found in the current attempt.")
+
+    def close_connection(self):
+        if self.client:
+            self.client.close()
+            self.client = None
+            logger.info("MongoDB connection closed.")
+
+    @staticmethod
+    def process_id(id_value):
+        try:
+            return ObjectId(id_value)
+        except Exception as e:
+            logger.error(f"Invalid ObjectId: {e}")
+            return None
+
+    @database_connection
+    def create_document(self, collection_name, new_document):
+        if not isinstance(new_document, dict):
+            logger.error("Invalid document format. Document should be a dictionary.")
+            return None
+
+        try:
+            collection = self.client[self.db_name][collection_name]
+            return collection.insert_one(new_document).inserted_id
+        except PyMongoError as e:
+            logger.error(f"Error adding document to MongoDB {collection_name} collection: {e}")
+            return None
+
+    @database_connection
+    def read_documents(self, collection_name, _id=None):
+        try:
+            collection = self.client[self.db_name][collection_name]
+
+            if _id:
+                _id = self.process_id(_id)
+                return collection.find_one({'_id': _id}) if _id else None
+
+            return list(collection.find())
+        except PyMongoError as e:
+            logger.error(f"Error retrieving document(s) from MongoDB {collection_name} collection: {e}")
+            return None if _id else []
+
+    @database_connection
+    def update_document(self, collection_name, document_id, update_data):
+        if not isinstance(update_data, dict):
+            logger.error("Invalid update data format. Data should be a dictionary.")
+            return False
+
+        document_id = self.process_id(document_id)
+        if not document_id:
+            return False
+
+        try:
+            collection = self.client[self.db_name][collection_name]
+            result = collection.update_one({'_id': document_id}, {'$set': update_data})
+            return result.matched_count > 0 and result.modified_count > 0
+        except PyMongoError as e:
+            logger.error(f"Error updating document in MongoDB {collection_name} collection: {e}")
+            return False
+
+    @database_connection
+    def delete_document(self, collection_name, document_id):
+        document_id = self.process_id(document_id)
+        if not document_id:
+            return False
+
+        try:
+            collection = self.client[self.db_name][collection_name]
+            result = collection.delete_one({'_id': document_id})
+            return result.deleted_count > 0
+        except PyMongoError as e:
+            logger.error(f"Error deleting document from MongoDB {collection_name} collection: {e}")
+
+
+def init_db(context):
+    if context and 'db' not in context:
+        load_dotenv()
+        context.db = Database()
+    return context.db
