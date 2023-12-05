a8f477c23a1e682b8adf64e6605c76b8ef654b19
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-12-05 22:24:44 +0100

    refactor(app): refactor app.py
    Refactored the app.py file to improve code structure and organization. Moved the import statements for the blueprints and config to separate lines for better readability. Also, initialized the MongoDB database within the app context.

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, g
 
-from blueprints.api_blueprint import api_blueprint
-from blueprints.htmx_blueprint import htmx_blueprint
+from blueprints.api_blueprint import api
+from blueprints.component_blueprint import component
 from config import DevelopmentConfig
+from mongodb import init_db
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
-app.register_blueprint(htmx_blueprint)
-app.register_blueprint(api_blueprint)
+
+with app.app_context():
+    db = init_db(g)
+    app.db = db
+
+app.register_blueprint(component)
+app.register_blueprint(api)
 
 
 if __name__ == '__main__':
