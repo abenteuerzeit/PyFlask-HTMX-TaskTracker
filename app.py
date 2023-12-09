cb018912a703c09281c07e9d8672005049bdb86a
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-12-09 13:03:43 +0100

    Refactor Flask application initialization and database setup

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
+"""
+This module initializes the Flask application, sets up the database connection, 
+and registers the blueprints for the application's components and API.
+"""
+
 from flask import Flask, g
 
 from blueprints.api_blueprint import api
 from blueprints.component_blueprint import component
 from config import DevelopmentConfig
-from mongodb import init_db
-
-app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
-
-with app.app_context():
-    db = init_db(g)
-    app.db = db
-
-app.register_blueprint(component)
-app.register_blueprint(api)
+from mongodb import Database, init_db
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+class FlaskApp(Flask):
+    db: Database
+
+
+def build():
+    app = FlaskApp(__name__)
+    app.config.from_object(DevelopmentConfig)
+    with app.app_context():
+        app.db = init_db(g)
+    app.register_blueprint(component)
+    app.register_blueprint(api)
+    return app
+
+
+if __name__ == "__main__":
+    flask_app = build()
+    flask_app.run(debug=True)
